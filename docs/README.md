d3f000ef854daf32a5d8423c9bac1bc482c22280
lwy <502051565@qq.com>
2022-11-14 09:07:50 +0800

    first commit

---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroText: xustudyxu's Blog
+tagline: 付出就会有收获，积跬步以至千里。
+pageClass: vdoing-index-class
+---
+
+<ClientOnly>
+  <WebInfo />
+ <IndexBigImg />
+</ClientOnly>
+
+
+
---
+++ docs/studynotes/about/sidebar.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    title:'关于',
+    collapsable: false,
+    children:[
+    '/studynotes/about/me',
+    '/studynotes/about/vuepress-build',
+    '/studynotes/about/github_skill',
+    '/@pages/archivesPage'
+      ]
+  }
+  
+]
